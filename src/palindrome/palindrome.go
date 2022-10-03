b01cde6bb33961980a1cbb340dc49d0d4abb0a34
William.Lieberz <williamlieberz@gmail.com>
2022-10-02 18:52:58 -0700

    Add palindrom func implementation

--- src/palindrome/palindrome.go
+++ src/palindrome/palindrome.go
@@ -1,6 +1,29 @@
 package palindrome
 
 func Palindrome(input string) bool {
-	//TODO!
+	var frontIdx int
+	var backIdx int
+	j := len(input)
+	i := 1
+
+	for i != j {
+		frontIdx = i - 1
+		backIdx = j - 1
+		frontChar := string([]rune(input)[frontIdx])
+		backChar := string([]rune(input)[backIdx])
+
+		if frontChar != backChar {
+			return false
+		}
+
+		// Prevent infinite loop: determine when we've compared all values:
+		if i+1 == j {
+			break
+		}
+
+		i += 1
+		j -= 1
+	}
+
 	return true
 }
