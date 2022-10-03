f4d6b87af90a9d6ce89ca56429336b9fc4bb68c6
William.Lieberz <williamlieberz@gmail.com>
2022-10-02 17:20:02 -0700

    Add tests

--- src/palindrome/palindrome_test.go
+++ src/palindrome/palindrome_test.go
@@ -11,3 +11,63 @@ func Test_aba_is_palindrome(t *testing.T) {
 		t.Fatalf("EXPECTED: %v GOT: %v", expected, output)
 	}
 }
+
+func Test_aba_space_before_not_palindrome(t *testing.T) {
+	input := " aba"
+	expected := false
+
+	output := Palindrome(input)
+	if output != expected {
+		t.Fatalf("EXPECTED: %v GOT: %v", expected, output)
+	}
+}
+
+func Test_aba_space_after_not_palindrome(t *testing.T) {
+	input := "aba "
+	expected := false
+
+	output := Palindrome(input)
+	if output != expected {
+		t.Fatalf("EXPECTED: %v GOT: %v", expected, output)
+	}
+}
+
+func Test_greetings_not_palindrome(t *testing.T) {
+	input := "greetings"
+	expected := false
+
+	output := Palindrome(input)
+	if output != expected {
+		t.Fatalf("EXPECTED: %v GOT: %v", expected, output)
+	}
+}
+
+func Test_numeric_is_palindrome(t *testing.T) {
+	input := "1000000001"
+	expected := true
+
+	output := Palindrome(input)
+	if output != expected {
+		t.Fatalf("EXPECTED: %v GOT: %v", expected, output)
+	}
+}
+
+func Test_fish_different_casing_not_palindrome(t *testing.T) {
+	input := "Fish hsif"
+	expected := false
+
+	output := Palindrome(input)
+	if output != expected {
+		t.Fatalf("EXPECTED: %v GOT: %v", expected, output)
+	}
+}
+
+func Test_pennep_is_palindrome(t *testing.T) {
+	input := "pennep"
+	expected := true
+
+	output := Palindrome(input)
+	if output != expected {
+		t.Fatalf("EXPECTED: %v GOT: %v", expected, output)
+	}
+}
