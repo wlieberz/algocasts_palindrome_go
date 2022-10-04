17a9c69d19f8b566c0b10b2c1324926032b95c16
William.Lieberz <williamlieberz@gmail.com>
2022-10-03 17:55:13 -0700

    Eliminate infinite loop   - The loop may end sooner, but worst-case it will   only ever need to iterate over the length of the input string.

--- src/palindrome/palindrome.go
+++ src/palindrome/palindrome.go
@@ -6,7 +6,7 @@ func Palindrome(input string) bool {
 	j := len(input)
 	i := 1
 
-	for i != j {
+	for rangeIdx, _ := range input {
 		frontIdx = i - 1
 		backIdx = j - 1
 		frontChar := string([]rune(input)[frontIdx])
@@ -16,13 +16,15 @@ func Palindrome(input string) bool {
 			return false
 		}
 
-		// Prevent infinite loop: determine when we've compared all values:
+		// If i is "one away" from j, we've compared all values:
 		if i+1 == j {
 			break
 		}
 
 		i += 1
 		j -= 1
+		// "consume" rangeIdx via discard to make the Go compiler happy:
+		_ = rangeIdx
 	}
 
 	return true
